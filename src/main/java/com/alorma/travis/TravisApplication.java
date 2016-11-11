96e1867954996b65f770a2ed40ed919690539ad9
Bernat Borrás <bernatbor15@gmail.com>
2016-11-11 17:44:08 +0100

    Initial commit

---
+++ src/main/java/com/alorma/travis/TravisApplication.java
@@ -0,0 +1,12 @@
+package com.alorma.travis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravisApplication.class, args);
+	}
+}
