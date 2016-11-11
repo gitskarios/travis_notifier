abfec526b9b8d5e596f02a0ae54ebd435f5f99a4
Bernat Borrás <bernatbor15@gmail.com>
2016-11-11 20:20:29 +0100

    Add logs

--- src/main/java/com/alorma/travis/view/MessagesController.java
+++ src/main/java/com/alorma/travis/view/MessagesController.java
@@ -3,6 +3,7 @@ package com.alorma.travis.view;
 import com.alorma.travis.domain.MessagesRepository;
 import com.alorma.travis.domain.TravisPayload;
 import com.alorma.travis.domain.response.MessageResponse;
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +20,24 @@ public class MessagesController {
     public
     @ResponseBody
     ResponseEntity<MessageResponse> handleMessage(@RequestBody TravisPayload event) throws Exception {
+        sendLog(event);
         return messagesRepository.sendMessage(event);
+    }
+
+    private void sendLog(TravisPayload event) {
+        if (event != null) {
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println("------------------------------------");
+            System.out.println("Travis POST:");
+            String json = new Gson().toJson(event);
+            System.out.println(json);
+            System.out.println("------------------------------------");
+            System.out.println();
+            System.out.println();
+            System.out.println();
+        }
     }
 
 }
